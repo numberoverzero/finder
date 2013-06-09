624854138d5bffee339b8c8c67ed4284dcac6a5c
Joe <joe.mcross@gmail.com>
2013-06-09 11:19:54 -0700

    db.create_all() so the table is created

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from finder import db
 
 
 class Card(db.Model):
-    __tablename__ = 'finder-cards'
+    __tablename__ = 'cards'
     id = db.Column(db.Integer, primary_key=True)
     multiverse_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.Text, nullable=False)
@@ -25,3 +25,32 @@ class Card(db.Model):
     calc_toughness = db.Column(db.Integer)
     calc_type = db.Column(db.Text)
     calc_subtypes = db.Column(db.Text)
+
+    def as_tabbed_value(self):
+        '''Returns the column values separated by tabs for bulk-importing.'''
+        return '\t'.join(
+            self.id,
+            self.multiverse_id,
+            self.name,
+            self.cost,
+            self.color,
+            self.type,
+            self.set,
+            self.rarity,
+            self.power,
+            self.toughness,
+            self.oracle_rules,
+            self.flavor_text,
+            self.watermark,
+            self.number,
+            self.artist,
+            self.calc_converted_mana_cost,
+            self.calc_colors,
+            self.calc_power,
+            self.calc_toughness,
+            self.calc_type,
+            self.calc_subtypes,
+        )
+
+
+db.create_all()
