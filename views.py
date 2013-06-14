5420d53f7f5024374b04d6a7818b3a5f24626f00
Joe <joe.mcross@gmail.com>
2013-06-14 00:06:26 -0700

    Cleanup views cruft (still a dummy method)

--- views.py
+++ views.py
@@ -1,19 +1,8 @@
 from flask.ext.restful import Resource, abort
-from finder.models import Card
 from finder import api
-#fields, marshal_with
-
-
-
-#gen_fields = {
-#    'key': fields.Raw,
-#    'salt': fields.Raw,
-#    'expiration': fields.Raw
-#}
 
 
 class Greet(Resource):
-    #@marshal_with(gen_fields)
     def get(self, name):
         if name in ['test']:
             abort(403, message="Must provide a real name.")
