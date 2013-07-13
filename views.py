fbd0d2c1598f305463cd349ac96f4ff108b90c1f
Joe <joe.mcross@gmail.com>
2013-07-13 04:52:42 -0700

    Test flask-restful reqparse module
    I'll eventually be using this for parsing the search string and
    various options (fields, dupes)

--- views.py
+++ views.py
@@ -1,10 +1,14 @@
-from flask.ext.restful import Resource, abort
+from flask.ext.restful import Resource, abort, reqparse
 from finder import api
+
+parser = reqparse.RequestParser()
+parser.add_argument('age', type=str)
 
 
 class Greet(Resource):
     def get(self, name):
+        args = parser.parse_args()
         if name in ['test']:
             abort(403, message="Must provide a real name.")
-        return {'msg': 'Hello, %s' % name}
+        return {'msg': 'Hello, %s' % name, 'age': args['age']}
 api.add_resource(Greet, '/hello/<string:name>')
