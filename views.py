bd3983ad25cdceec4778c4b45dcf0f7ffcb83c0a
Joe Cross <joe.mcross@gmail.com>
2013-08-15 20:29:12 -0700

    Add basic view to test card queries
    This verifies that jsonquery integration works, as well controller
    formatting and the scraper converting.

--- views.py
+++ views.py
@@ -1,9 +1,20 @@
 from flask import request, jsonify, abort
-from finder import app
-
+from finder import app, db, controllers, models
+from jsonquery import jsonquery
 
 @app.route('/echo', methods=["PUT", "POST"])
 def echo():
     if not request.json:
         abort(400)
     return jsonify(**request.json)
+
+@app.route('/card/<string:name>', methods=["GET"])
+def card(name):
+	query = {
+		'column': 'processed_ascii_name',
+		'operator': 'ilike',
+		'value': name
+	}
+	card = jsonquery(db.session, models.Card, query).first()
+	card_dict = controllers.with_fields(card)
+	return jsonify(**card_dict)
